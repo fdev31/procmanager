7f41f50bf116b0ee1753747334590b3b3e0480a0
fdev31 <fdev31@gmail.com>
2022-06-09 01:22:14 +0200

    version 1.0.1

--- setup.py
+++ setup.py
@@ -2,9 +2,10 @@ from setuptools import setup
 
 setup(
    name='procmanager',
-   version='1.0',
+   version='1.0.1',
    description='Helps starting & stopping programs',
    author='Fabien Devaux',
+   url='https://github.com/fdev31/procmanager',
    author_email='fdev31@gmail.com',
    packages=['procmanager'],  # would be the same as name
    scripts=['scripts/procmgr'],
